eec397e8371844c8f069d99a8bd7e3232691a3f8
gabe marquez <amarquez727@gmail.com>
2014-12-22 00:48:09 -0500

    plz work

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn restaurant_db:app --log-file=-
--- restaurant_db/__init__.py
+++ restaurant_db/__init__.py
@@ -4,10 +4,6 @@ app = Flask(__name__)
 app.secret_key="gabe and kevin"
 
 import restaurant_db.routes
-from restaurant_db.rdb import db
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rdb:rdb@localhost/Food'
-
-db.init_app(app)
-
 
